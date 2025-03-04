496fe09a52f9ab7ba2b68fe4aa8cd170e62d3a77
GeorgeSantos7 <georgedossantosramos@gmail.com>
2025-03-04 11:01:35 -0300

    Primeiro commit na branch dev

---
+++ src/main/java/com/desbravadores/terras/TerrasApplication.java
@@ -0,0 +1,13 @@
+package com.desbravadores.terras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desbravadores/terras/TerrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desbravadores.terras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
